7a842434713ef8c7e74141b01a5a8494aced0dc0
Earlz <earlz@lastyearswishes.com>
2013-01-22 23:44:14 -0500

    Added 2 more unit tests

--- BarelyMVC.Tests/FSCAuthTests.cs
+++ BarelyMVC.Tests/FSCAuthTests.cs
@@ -4,6 +4,7 @@ using Earlz.BarelyMVC.Authentication;
 using Moq;
 using Earlz.BarelyMVC;
 using System.Web;
+using System.Collections.Generic;
 
 namespace BarelyMVC.Tests
 {
@@ -18,10 +19,10 @@ namespace BarelyMVC.Tests
 			FSCAuth.Config.SiteName="test";
 			FSCAuth.Config.Server=null; //must be mocked
 			FSCAuth.UserStore=new SimpleUserStore();
-			FSCAuth.AddUser(new UserData(){Username="user"}, "pass");
+			FSCAuth.AddUser(new UserData(){Username="user", Groups=new List<GroupData>(){new GroupData("foobar")}}, "pass");
 		}
 		[Test]
-		public void RequiresLoginShouldRedirect()
+		public void RequiresLogin_should_redirect_when_not_logged_in()
 		{
 			var mock=new Mock<IServerContext>();
 			FSCAuth.Config.Server=mock.Object;
@@ -35,7 +36,7 @@ namespace BarelyMVC.Tests
 			mock.Verify(x=>x.Redirect("/login"), "Should redirect to login page");
 		}
 		[Test]
-		public void LoginShouldWork()
+		public void Login_should_work_when_given_proper_credentials()
 		{
 			var mock=new Mock<IServerContext>();
 			FSCAuth.Config.Server=mock.Object;
@@ -46,6 +47,23 @@ namespace BarelyMVC.Tests
 			         c.Values["secret"].Length>0
             	)
 			));
+		}
+		[Test]
+		public void RequiresLogin_should_pass_through_when_authorized()
+		{
+			var mock=new Mock<IServerContext>();
+			FSCAuth.Config.Server=mock.Object;
+			HttpCookie logincookie=null;
+			mock.Setup(x=>x.AddCookie(
+				It.Is<HttpCookie>(c=>true) //don't care about the parameter specification
+			)).Callback<HttpCookie>(cookie=>logincookie=cookie);
+			FSCAuth.Login("user", "pass", false);
+			Assert.NotNull(logincookie);
+
+			mock.Setup(x=>x.GetCookie(FSCAuth.Config.SiteName+"_login")).Returns(logincookie).Verifiable();
+			mock.Setup(x=>x.KillIt()).Throws(new ApplicationException("KillIt should not be called as this indicates that the login did not succeed"));
+			FSCAuth.RequiresLogin();
+			mock.Verify();
 		}
 	}
 }
