58f73b41ece8b64d22fdf5da270c77973be6bbd2
Earlz <earlz@lastyearswishes.com>
2013-02-06 02:22:26 -0500

    Added MockServerContext. Not sure if that will get "complete", but it seems lie a good idea, maybe

---
+++ BarelyMVC/TestingUtilities/MockServerContext.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Web;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+
+namespace Earlz.BarelyMVC.TestingUtilities
+{
+	/// <summary>
+	/// This is a mock IServerContext. This is to make unit testing easier on your fingers 
+	/// Get/Set Item/cookie/Header stuff just passes through to dictionaries. Properties include set methods so you can set them manually if needed
+	/// </summary>
+	public class MockServerContext : IServerContext
+	{
+		public List<HttpCookie> InputCookies=new List<HttpCookie>();
+		public HttpCookie GetCookie (string name)
+		{
+			throw new NotImplementedException();
+		}
+		public List<HttpCookie> OutputCookies=new List<HttpCookie>();
+		public void SetCookie (HttpCookie cookie)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void KillIt ()
+		{
+			throw new StopExecutionException();
+		}
+
+		public string MapPath (string path)
+		{
+			return "/foo/bar/"+path;
+		}
+		public List<KeyValuePair<string, string>> InputHeaders=new List<KeyValuePair<string, string>>();
+		public string GetHeader (string name)
+		{
+			throw new NotImplementedException ();
+		}
+		public List<KeyValuePair<string, string>> OutputHeaders=new List<KeyValuePair<string, string>>();
+		public void SetHeader (string name, string value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void Transfer (string url)
+		{
+			throw new StopExecutionException();
+		}
+
+		public void Redirect (string url)
+		{
+			throw new StopExecutionException();
+		}
+		public ConcurrentDictionary<string, object> Items=new ConcurrentDictionary<string, object>();
+		public object GetItem (string name)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public object SetItem (string name, object value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public string UserAgent {
+			get;
+			set;
+		}
+
+		public string UserIP {
+			get;
+			set;
+		}
+
+		public string HttpStatus {
+			get;
+			set;
+		}
+
+		public TextWriter Writer {
+			get;
+			set;
+		}
+
+		public MockServerContext ()
+		{
+		}
+	}
+}
+
